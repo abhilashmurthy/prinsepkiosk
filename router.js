2d1e4ae666dc4a5d9f238724850d3a3fbb5e128a
Abhilash Murthy <abhilashm.2010@sis.smu.edu.sg>
2014-02-08 21:29:33 +0800

    Fixed broken image error - Replaced with link from Router

--- router.js
+++ router.js
@@ -16,3 +16,18 @@ Router.map(function() {
 		path: '/map'
 	});
 });
+
+if (Meteor.isServer) {
+	Router.map(function() {
+	  return this.route('showImage', {
+		path: '/show-image/:imageId',
+		method: 'GET',
+		where: "server",
+		action: function() {
+		  this.response.writeHead (200, {'Content-type': 'image/png'});
+		  var buffer = ItemsFS.retrieveBuffer (this.params.imageId);
+		  return this.response.end (buffer);
+		}
+	  });
+	});
+}
